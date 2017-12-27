e972841f90466fb9010f4def4a6594bdcc43ac77
alejandraHoces <hocesalejandra@gmail.com>
2017-12-26 20:54:20 -0500

    Añadiendo estructura html,css y datajs

---
+++ js/data.js
@@ -0,0 +1,58 @@
+var restaurants = [
+  {
+    name: "Sr. Limón",
+    address: ["Av. Javier Prado Este 5335, La Molina", "Av.Industrial s/n, Independencia"],
+    filters: ["Pescados", "Ceviche", "Mariscos"],
+    photo: "<img src='assets/images/cevicheria.jpg' alt='Sr. Limón' class='mouseover'>",
+    website: "https://www.facebook.com/senorlimonoficial/"
+  },
+  {
+    name: "Don Belisario",
+    address: ["C.C Plaza Norte, Tomás Valle con Panamericana Norte, Independencia"],
+    filters: ["Parrilladas", "Pollos a la brasa", "Delivery"],
+    photo: "<img src='assets/images/donbelisario.jpg' alt='Don Belisario' class='mouseover'>",
+    website: "http://www.donbelisario.com.pe"
+  },
+  {
+    name: "Puro Perú",
+    address: ["Av. República de Panamá 258, Barranco"],
+    filters: ["Fusión Peruana", "Criolla"],
+    photo: "<img src='assets/images/puroperu.jpg' alt='Puro Perú' class='mouseover'>",
+    website: "http://puroperu.com.pe/es/"
+  },
+  {
+    name: "El Aguajal",
+    address: ["Av. Carlos Izaguirre 15301, Los Olivos"],
+    filters: ["Platos tipicos", "Comida Selvatica"],
+    photo: "<img src='assets/images/lachoza.jpg' alt='La choza de la Anaconda' class='mouseover'>",
+    website: "http://www.aguajal.com/"
+  },
+  {
+    name: "Pizza Rock",
+    address: ["Av.Del ejercito 695, Miraflores", "CC.Plaza Lima Norte, Independecia", "Av.Belisario 1007, San Isidro"],
+    filters: ["Pizza", "Comida Rapida", "Delivery"],
+    photo: "<img src='assets/images/pizzarock.jpg' alt='Pizza Rock' class='mouseover'>",
+    website: "http://www.pizzarock.pe/"
+  },
+  {
+    name: "Papachos",
+    address: ["CC.Larcomar, Miraflores", "CC.Real Plaza Salaverry, Jesus Maria", "Av.La Paz 1045, Miraflores", "CC.Jockey Plaza, Surco"],
+    filters: ["Comida Rapida", "Salchipapas"],
+    photo: "<img src='assets/images/papachos.jpg' alt='Papachos' class='mouseover'>",
+    website: "https://papachos.com/"
+  },
+  {
+    name: "Makoto Sushi Bar",
+    address: ["Av.2 de mayo 585, San Isidro"],
+    filters: ["Sushi", "Fusión Peruana"],
+    photo: "<img src='assets/images/makoto.jpg' alt='Makoto' class='mouseover'>",
+    website: "https://www.makotosushibar.com/"
+  },
+  {
+    name: "Cafeteria Altomayo",
+    address: ["Aeropuerto Jorge Chavez, Vía Expresa Elmer Faucett s/n, Callao"],
+    filters: ["Café"],
+    photo: "<img src='assets/images/altomayocafe.jpg' alt='Cafeteria Altomayo' class='mouseover'>",
+    website: "http://www.cafealtomayo.com/"
+  }
+];
