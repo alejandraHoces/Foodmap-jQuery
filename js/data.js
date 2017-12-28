879d62944753fe67b0c34e916435286afec777a9
alejandraHoces <hocesalejandra@gmail.com>
2017-12-28 14:59:50 -0500

    Añadiendo archivo js

--- js/data.js
+++ js/data.js
@@ -1,58 +1,72 @@
 var restaurants = [
   {
-    name: "Sr. Limón",
-    address: ["Av. Javier Prado Este 5335, La Molina", "Av.Industrial s/n, Independencia"],
-    filters: ["Pescados", "Ceviche", "Mariscos"],
+    name: 'Sr. Limón',
+    address: ['Av. Javier Prado Este 5335, La Molina', 'Av.Industrial s/n, Independencia'],
+    filters: ['Pescados'],
     photo: "<img src='assets/images/cevicheria.jpg' alt='Sr. Limón' class='mouseover'>",
-    website: "https://www.facebook.com/senorlimonoficial/"
+    website: 'https://www.facebook.com/senorlimonoficial/'
   },
   {
-    name: "Don Belisario",
-    address: ["C.C Plaza Norte, Tomás Valle con Panamericana Norte, Independencia"],
-    filters: ["Parrilladas", "Pollos a la brasa", "Delivery"],
+    name: 'Don Belisario',
+    address: ['C.C Plaza Norte, Tomás Valle con Panamericana Norte, Independencia'],
+    filters: ['Parrilladas', 'Delivery' ,'Pollos a la Brasa'],
     photo: "<img src='assets/images/donbelisario.jpg' alt='Don Belisario' class='mouseover'>",
-    website: "http://www.donbelisario.com.pe"
+    website: 'http://www.donbelisario.com.pe'
   },
   {
-    name: "Puro Perú",
-    address: ["Av. República de Panamá 258, Barranco"],
-    filters: ["Fusión Peruana", "Criolla", "Platos tipicos"],
+    name: 'Puro Perú',
+    address: ['Av. República de Panamá 258, Barranco'],
+    filters: ['Fusión Peruana', 'Criolla', 'Platos tipicos'],
     photo: "<img src='assets/images/puroperu.jpg' alt='Puro Perú' class='mouseover'>",
-    website: "http://puroperu.com.pe/es/"
+    website: 'http://puroperu.com.pe/es/'
   },
   {
-    name: "El Aguajal",
-    address: ["Av. Carlos Izaguirre 15301, Los Olivos"],
-    filters: ["Platos tipicos", "Comida Selvatica"],
+    name: 'El Aguajal',
+    address: ['Av. Carlos Izaguirre 15301, Los Olivos'],
+    filters: ['Platos tipicos', 'Comida Selvatica'],
     photo: "<img src='assets/images/lachoza.jpg' alt='La choza de la Anaconda' class='mouseover'>",
-    website: "http://www.aguajal.com/"
+    website: 'http://www.aguajal.com/'
   },
   {
-    name: "Pizza Rock",
-    address: ["Av.Del ejercito 695, Miraflores", "CC.Plaza Lima Norte, Independecia", "Av.Belisario 1007, San Isidro"],
-    filters: ["Pizza", "Comida Rapida", "Delivery"],
+    name: 'Pizza Rock',
+    address: ['Av.Del ejercito 695, Miraflores', 'CC.Plaza Lima Norte, Independecia', 'Av.Belisario 1007, San Isidro'],
+    filters: ['Pizza', 'Comida Rapida', 'Delivery'],
     photo: "<img src='assets/images/pizzarock.jpg' alt='Pizza Rock' class='mouseover'>",
-    website: "http://www.pizzarock.pe/"
+    website: 'http://www.pizzarock.pe/'
   },
   {
-    name: "Papachos",
-    address: ["CC.Larcomar, Miraflores", "CC.Real Plaza Salaverry, Jesus Maria", "Av.La Paz 1045, Miraflores", "CC.Jockey Plaza, Surco"],
-    filters: ["Comida Rapida", "Salchipapas"],
+    name: 'La choza Nautica',
+    address: ['Jr.Zeus 809', 'Los Olivos'],
+    filters: ['Ceviche', 'Mariscos'],
+    photo: "<img src='assets/images/arrozconmarisco.jpg' alt='Sr. Limón' class='mouseover'>",
+    website: 'http://www.chozanautica.com'
+  },
+  {
+    name: 'Papachos',
+    address: ['CC.Larcomar, Miraflores', 'CC.Real Plaza Salaverry, Jesus Maria', 'Av.La Paz 1045, Miraflores', 'CC.Jockey Plaza, Surco'],
+    filters: ['Comida Rapida', 'Salchipapas', 'Hamburguesas'],
     photo: "<img src='assets/images/papachos.jpg' alt='Papachos' class='mouseover'>",
-    website: "https://papachos.com/"
+    website: 'https://papachos.com/'
   },
   {
-    name: "Makoto Sushi Bar",
-    address: ["Av.2 de mayo 585, San Isidro"],
-    filters: ["Sushi", "Fusión Peruana"],
+    name: 'Makoto Sushi Bar',
+    address: ['Av.2 de mayo 585, San Isidro'],
+    filters: ['Sushi', 'Fusión Peruana'],
     photo: "<img src='assets/images/makoto.jpg' alt='Makoto' class='mouseover'>",
-    website: "https://www.makotosushibar.com/"
+    website: 'https://www.makotosushibar.com/'
   },
   {
-    name: "Cafeteria Altomayo",
-    address: ["Aeropuerto Jorge Chavez, Vía Expresa Elmer Faucett s/n, Callao"],
-    filters: ["Café"],
+    name: 'La Leña',
+    address: ['Las Violetas 772, Independencia'],
+    filters: ['Pollos a la Brasa', 'Parrilladas'],
+    photo: "<img src='assets/images/laleña.jpg' alt='Don Belisario' class='mouseover'>",
+    website: 'http://www.lalena.com.pe'
+  },
+  {
+    name: 'Cafeteria Altomayo',
+    address: ['Aeropuerto Jorge Chavez, Vía Expresa Elmer Faucett s/n, Callao'],
+    filters: ['Café'],
     photo: "<img src='assets/images/altomayocafe.jpg' alt='Cafeteria Altomayo' class='mouseover'>",
-    website: "http://www.cafealtomayo.com/"
+    website: 'http://www.cafealtomayo.com/'
   }
 ];
