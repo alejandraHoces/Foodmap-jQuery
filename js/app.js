88b93a48ab9dcffc732f732e34ad878a16908eeb
alejandraHoces <hocesalejandra@gmail.com>
2017-12-30 00:47:08 -0500

    Añadiendo archivo js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
-// funcion que coloca todos los filtros de comida en las opciones de seleccion
-var addFilters = (function(element) {
-  var filtersArr = [];
-  for (var i = 0; i < restaurants.length; i++) {
-    for (var n = 0; n < restaurants[i].filters.length; n++) {
-      filtersArr.push(restaurants[i].filters[n]);
-    }
-  }
-  var uniqueFilters = [...new Set(filtersArr)];
-  var filtersFinal = uniqueFilters.sort();
-  for (var a = 0; a < filtersFinal.length; a++) {
-    element.append("<option value='"+filtersFinal[a]+"'>"+filtersFinal[a]+"");
-  };
-  return filtersFinal;
-});
-
-
 $(document).ready(function() {
+  // funcion que coloca todos los filtros de comida en las opciones de seleccion
+  var addFilters = (function(element) {
+    var filtersArr = [];
+    for (var i = 0; i < restaurants.length; i++) {
+      for (var n = 0; n < restaurants[i].filters.length; n++) {
+        filtersArr.push(restaurants[i].filters[n]);
+      }
+    }
+    var uniqueFilters = [...new Set(filtersArr)];
+    var filtersFinal = uniqueFilters.sort();
+    for (var a = 0; a < filtersFinal.length; a++) {
+      element.append("<option value='"+filtersFinal[a]+"'>"+filtersFinal[a]+"");
+    };
+    return filtersFinal;
+  });
   // Funcion que crea nuevas opciones
   addFilters($('#filter'));
 
